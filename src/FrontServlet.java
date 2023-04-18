fbbc32ed29a241a9538e8c56bf01fd64df1758ee
JudiToks <judi.rakotoarisedy@gmail.com>
2023-04-18 09:32:43 +0300

    sprint 2 finish

--- src/FrontServlet.java
+++ src/FrontServlet.java
@@ -2,14 +2,19 @@ package etu1820.framework.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class FrontServlet extends HttpServlet {
-    
+import etu1820.framework.Mapping;
+
+public class FrontServlet extends HttpServlet 
+{
+    HashMap<String, Mapping> mappingUrls;
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
---
+++ src/Mapping.java
@@ -0,0 +1,33 @@
+package etu1820.framework;
+
+public class Mapping 
+{
+    String className;
+    String method;
+
+    // getters & setters
+    public String getClassName() 
+    {
+        return className;
+    }
+    public void setClassName(String className) 
+    {
+        this.className = className;
+    }
+    public String getMethod() 
+    {
+        return method;
+    }
+    public void setMethod(String method) 
+    {
+        this.method = method;
+    }
+
+    // constructor
+    public Mapping() {}
+    public Mapping(String className, String method) 
+    {
+        this.setClassName(className);
+        this.setMethod(method);
+    }
+}
